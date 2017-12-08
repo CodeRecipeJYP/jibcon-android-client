24aa45e8377783bba7af62e446eb149d926b505d
pjo901018 <pjo901018@korea.ac.kr>
2017-12-08 10:19:34 +0900

    Removing LoginActivity Template / Adding SigningActivity

---
+++ app/src/main/java/com/asuscomm/smarts/jibcon_client_android/SigningActivity.java
@@ -0,0 +1,13 @@
+package com.asuscomm.smarts.jibcon_client_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signing);
+    }
+}
