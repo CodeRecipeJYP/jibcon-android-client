520ae8adec8b68ade61fff9eceb8bc0ed20638c2
pjo901018 <pjo901018@korea.ac.kr>
2017-12-08 19:54:56 +0900

    Adding firebase-auth / Enabling FacebookSigning

--- app/src/main/java/com/asuscomm/smarts/jibcon_client_android/SigningActivity.java
+++ app/src/main/java/com/asuscomm/smarts/jibcon_client_android/SigningActivity.java
@@ -1,13 +1,67 @@
 package com.asuscomm.smarts.jibcon_client_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SigningActivity extends AppCompatActivity {
+    private static final String TAG = "SigningActivity";
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signing);
+
+        Log.d(TAG, "onCreate: ");
+
+        startAuthUI();
     }
+
+    private void startAuthUI() {
+        Log.d(TAG, "startAuthUI: ");
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+//                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == ResultCodes.OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: user=" + user);
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+                Log.d(TAG, "onActivityResult: signin failed");
+            }
+        }
+    }
+
 }
